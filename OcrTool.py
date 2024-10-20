64f18bc2fef83752ccf5dea29ee259b84ddb5a77
JB <jababbel@gmail.com>
2024-10-20 14:29:27 +0200

    use psm 6

--- OcrTool.py
+++ OcrTool.py
@@ -7,5 +7,6 @@ class OcrTool:
         self.tesseract_executable_path = tesseract_executable_path
 
     def get_hocr(self, image_path):
-        hocr_output = subprocess.getoutput(f'"{self.tesseract_executable_path}" {image_path} - --psm 4 hocr')
+        # --psm 6 should be slightly superior to --psm 4 here because the text size does not vary much (more testing needed)
+        hocr_output = subprocess.getoutput(f'"{self.tesseract_executable_path}" {image_path} - --psm 6 hocr')
         return hocr_output
